56ce9a6145285bf75b96ea2ccca23f73b0117dae
Omar Vayani <vayaniumar@gmail.com>
2022-07-16 02:38:22 +0200

    finished project

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
-  .then((result) => result.json())
-  .then((data) => console.log(data.data[0].quoteText));
+const btn = document.querySelector("button");
+const h2 = document.querySelector("h2");
+
+async function getQuote() {
+  let quote = await fetch(
+    "https://quote-garden.herokuapp.com/api/v3/quotes/random"
+  )
+    .then((result) => result.json())
+    .then((data) => data.data[0]);
+  console.log(quote);
+
+  h2.innerHTML = `"${quote.quoteText}"<br>-${quote.quoteAuthor}`;
+}
+
+btn.addEventListener("click", () => {
+  getQuote();
+});
